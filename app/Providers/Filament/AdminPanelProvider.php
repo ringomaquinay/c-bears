348abd5aad51ead8496606157ad000576667bd00
Ringo Maquinay <gringomaquinay@gmail.com>
2026-09-20 01:44:20 +0800

    Update C-BEARS admin panel brand text

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->brandName('C-BEARS')
+            ->brandName('C-BEARS | Building Assessment System')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
